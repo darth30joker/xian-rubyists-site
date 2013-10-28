a81b36bd4ee6c15d3704d966fee3d74e0615b4ee
Hao Hu <huhao@outlook.com>
2013-10-28 21:36:58 +0800

    Create new rails4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XianRubyistsSite::Application.config.secret_key_base = '08802e6651adb1e2916d5483876a7ed6f82801af58a9d79ed88f9dce4fe79c93356eb170b28f5ca3f1a08174a7d2b496d074edc1af0efc732537ef0a2fa08d22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XianRubyistsSite::Application.config.session_store :cookie_store, key: '_xian-rubyists-site_session'
