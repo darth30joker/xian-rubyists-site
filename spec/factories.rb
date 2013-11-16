8bd883cf9f3cebb3503e47cead72d20af8e07cad
Hao Hu <huhao@outlook.com>
2013-11-16 10:29:56 +0800

    Change back to faker gem

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,15 @@
-require 'ffaker'
+require 'faker'
+
+def random_date(from = 0.0, to = Time.now)
+  Time.at(from + Random.rand * (to.to_f - from.to_f)).to_date
+end
 
 FactoryGirl.define do
   factory :user do
-    name { Faker::Internet.user_name }
+    name { Faker::Internet.user_name(nil, %w(- _)) }
     fullname { Faker::Name.name }
     email { Faker::Internet.email }
-    birthday Faker::Time.date(year_range:25, year_latest: 18)
+    birthday random_date(Time.new(1970, 1, 1), Time.new(1995, 1, 1))
     password 'xianrubyist'
     password_confirmation 'xianrubyist'
   end
